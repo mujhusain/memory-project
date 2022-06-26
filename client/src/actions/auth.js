ba7e2d62a12b80b407bd6d06a9f186a0cc4795a2
mujaffar <mujaffarhssn@gmail.com>
2022-06-26 18:58:45 +0530

    google login and manual login signup frontend part created

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,16 +1,16 @@
 import { AUTH } from "../constants/actionTypes";
 import * as api from "../api";
 
-export const signin = ((formData, navigate) = async (dispatch) => {
+export const signin = (formData, navigate) => async (dispatch) => {
   try {
     // ..login first thn navigate to home page
     navigate("/");
   } catch (err) {
     console.log(err);
   }
-});
+};
 
-export const signup = ((formData, navigate) = async (dispatch) => {
+export const signup = (formData, navigate) => async (dispatch) => {
   try {
     // ..login part
 
@@ -18,4 +18,4 @@ export const signup = ((formData, navigate) = async (dispatch) => {
   } catch (err) {
     console.log(err);
   }
-});
+};
